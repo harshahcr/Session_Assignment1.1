62c7895412909348f83627f58a1f21d2d263ca7b
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-09 11:37:23 +0530

    Add files via upload

---
+++ Assignment_1 session_1.py
@@ -0,0 +1,46 @@
+#Hello World - String program
+print ('Hello World!')
+
+welcome = ('Starting the journey of Python developer in the world of data science')
+print(welcome)
+
+#Sum of integers
+x=2
+y=3
+a=x+y
+print('Sum of interger value is', a)
+
+#Sum of float numbers
+x=2.35;y=10.85
+b=x+y
+print('Sum of floating numbers is', b)
+
+#Multiplication of integers numbers
+x=8;y=10
+c=x*y
+print('Multiplication of integer numbers is', c)
+
+#Multiplication of float numbers
+x=5.25;y=10.85
+d=x*y
+print('Multiplication of floating numbers is', d)
+
+#Division of integers numbers
+x=200;y=5
+e=x/y
+print('Division of integer numbers is', e)
+
+#Division of float numbers
+x=80.35;y=10.85
+f=x/y
+print('Division of floating numbers is', f)
+
+
+print('Value stored in variable a=',a)
+print('Value stored in variable b=',b)
+print('Value stored in variable c=',c)
+print('Value stored in variable d=',d)
+print('Value stored in variable e=',e)
+print('Value stored in variable f=',f)
+
+
