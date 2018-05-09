d2401d6e05b84cb9f1591b149ed922e86c4f82ac
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-09 11:37:39 +0530

    Add files via upload

---
+++ List container - program.py
@@ -0,0 +1,34 @@
+#List - container programs
+
+#Simple print on list 
+var = ['a','b','c','d']
+varlist = list(var)
+print(list(varlist))
+print(var)
+
+#Python string is also a sequence of characters
+strlist = list('Harsha')
+print(strlist)
+
+#index concepts
+l = []
+l.append('H')
+l.append('a')
+l.append('r')
+l.append('s')
+l.append('h')
+l.append('a')
+print(l)
+      
+#insert content to list
+
+il=[]
+il.append('a')
+il.append('r')
+il.append('h')
+il.append('a')
+il.insert(0,'H')
+il.insert(3,'s')
+print('Alphabets are appended in list to spell my name',il)
+
+
